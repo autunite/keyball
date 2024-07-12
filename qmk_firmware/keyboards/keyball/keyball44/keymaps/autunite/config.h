2bbe9862e3c4c25aeca745e5167aa29bc1e87b28
autunite <_@autunite.dev>
2024-07-12 13:49:17 +0900

    スペルミスの修正と複合キー同時押しの処理の変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/autunite/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/autunite/config.h
@@ -38,6 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
-#define TAPPING_ITEM 100
+#define TAPPING_TERM 200
+#define HOLD_ON_OTHER_KEY_PRESS
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
